efdf964b33f1bf14fdd05ef296356f50ec5d7e61
王文明 <wangwenming1985@gmail.com>
2020-08-19 16:22:19 +0800

    日志追踪1.0

--- src/main/java/com/quanfangtongvip/springcloudplayground/controller/HelloController.java
+++ src/main/java/com/quanfangtongvip/springcloudplayground/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.quanfangtongvip.springcloudplayground.controller;
 
+import com.quanfangtongvip.springcloudplayground.context.enums.ErrorCode;
+import com.quanfangtongvip.springcloudplayground.context.exception.BaseException;
+import com.quanfangtongvip.springcloudplayground.context.response.Result;
+import com.quanfangtongvip.springcloudplayground.service.HelloService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -11,8 +17,12 @@ import java.io.IOException;
 @RequestMapping("/hello")
 @Slf4j
 public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping("/")
-    public String get() {
-        return "hello";
+    public Result get(@RequestParam(value="name") String name) {
+        return helloService.sayHello(name);
     }
 }
